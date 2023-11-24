aff5a2fa63674e00d1245380025331884c218ea3
ARIF AQEEL AHMAD <arifaqeelahmad059@gmail.com>
2023-11-24 17:41:33 +0500

    insertion at start

---
+++ Linked_List/Insertion_start.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+struct insert_start
+{
+    int info;
+    insert_start *link;
+};
+insert_start *head = NULL, *temp;
+
+void display()
+{
+    insert_start *ptr;
+    ptr = head;
+    while (ptr != NULL)
+    {
+        cout << ptr->info << " ,";
+        ptr = ptr->link;
+    }
+}
+
+int main()
+{
+    head = new insert_start();
+    head->info = 20;
+    head->link = NULL;
+
+    insert_start *head2 = new insert_start(); // 2nd Node
+    head2->info = 100;
+    head2->link = NULL;
+    head->link = head2;
+
+    // let we want to insert a value at the start
+
+    int num;
+    cout << "ENTER VALUE YOU WANT TO INSERT AT START: ";
+    cin >> num;
+
+    insert_start *newNode = new insert_start();
+    newNode->info = num;
+    newNode->link = NULL;
+
+    temp = head;
+    newNode->link = temp;
+    head = newNode;
+
+    display();
+
+    return 0;
+}
