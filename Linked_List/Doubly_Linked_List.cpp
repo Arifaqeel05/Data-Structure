27ee77d0c3f1a60ac69f15cca6f48a183171f412
ARIF AQEEL AHMAD <arifaqeelahmad059@gmail.com>
2023-11-25 18:11:37 +0500

    Update Doubly_Linked_List.cpp

--- Linked_List/Doubly_Linked_List.cpp
+++ Linked_List/Doubly_Linked_List.cpp
@@ -11,22 +11,25 @@ Node *head;
 
 void creating_list()
 {
-    int choice = 1; // Changed to int and initialized to 1
+
     head = NULL;
     Node *temp, *newNode;
+    int choice = 1; // Changed to int and initialized to 1
     while (choice != 0)
     {
         int number;
         cout << "\nENTER NUMBER YOU WANT TO INSERT IN LIST: ";
         cin >> number;
-        temp = new Node();
+        temp = new Node(); // Memory Allocation
+
         temp->data = number;
         temp->previous = NULL;
         temp->next = NULL;
 
+        // checking eaither it is 1st Node if yes then if-part will execute
         if (head == NULL)
         {
-            head = temp;
+            head = temp;    // Initialize head when head is NULL
             newNode = temp; // Initialize newNode when head is NULL
         }
         else
